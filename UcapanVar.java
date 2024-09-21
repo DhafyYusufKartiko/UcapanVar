7a6c5698f1f556bf3bad38cbc6c077501a1a2976
DhafyYusufKartiko <dhafyyusuf@gmail.com>
2024-09-21 13:47:11 +0700

    Add files via upload

---
+++ UcapanVar.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author Tiko ingin bisa ngoding
+ */
+public class UcapanVar {
+
+    public static void main(String[] args) {
+        String univ = "ULM";
+        String visiUniv = "Berdaya Saing dan Unggul di Bidang Lahan Basah";
+        String prodi = "Ilmu komputer";
+        String visiIlkom = "Unggul dengan Teknologi Kekinian";
+
+        System.out.println("Halo " + univ);
+        System.out.println(univ + " " + visiUniv);
+        System.out.println("Prodi " + prodi + " " + visiIlkom);
+        System.out.println("------");
+        System.out.println("Halo " + univ + "\n" + univ + " " + visiUniv + "\n" + "Prodi " + prodi + " " + visiIlkom);
+
+    }
+
+}
